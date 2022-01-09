00130d253be7308529718205f1f70ee50085b847
James Leahy <defuncart@gmail.com>
2022-01-09 15:33:54 +0100

    Align pseudoloca_generate.sh with loca_generate.sh

--- bin/pseudoloca_generate.sh
+++ bin/pseudoloca_generate.sh
@@ -4,18 +4,21 @@
 # if already enabled, this will download latest version, if one is available
 dart pub global activate arb_utils
 
+# ensure all deps are available
+dart pub get
+
 # generate arb files
-flutter pub run arb_generator
+dart run arb_generator
 
 # sort arbs
-pub global run arb_utils:sort assets_dev/l10n/intl_en.arb
-pub global run arb_utils:sort assets_dev/l10n/intl_de.arb
+dart pub global run arb_utils:sort assets_dev/l10n/intl_en.arb
+dart pub global run arb_utils:sort assets_dev/l10n/intl_de.arb
 
 # generate pseudo localizations
-flutter pub run flutter_pseudolocalizor
+dart run flutter_pseudolocalizor
 
 # generate localization delegates
-flutter pub run intl_utils:generate
+dart run intl_utils:generate
 
 # format loca delegates
-flutter format lib/generated
+dart format lib/generated
